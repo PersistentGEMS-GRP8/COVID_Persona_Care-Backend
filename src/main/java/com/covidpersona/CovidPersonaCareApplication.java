fbd2d8b7b776fdfcba385d9d1841440a7799496a
Binuri <binurim95@gmail.com>
2021-07-30 14:20:32 +0530

    Intial commit

---
+++ src/main/java/com/covidpersona/CovidPersonaCareApplication.java
@@ -0,0 +1,13 @@
+package com.covidpersona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidPersonaCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidPersonaCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/covidpersona/CovidPersonaCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covidpersona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidPersonaCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
