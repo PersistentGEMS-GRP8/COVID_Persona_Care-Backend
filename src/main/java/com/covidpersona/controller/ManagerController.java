55da7d46a07b094cce8a1e170d0e0bbc52a74f11
Dilsha Hettiarachchi <dilshanavodi@gmail.com>
2021-08-04 16:24:05 +0530

    manager's CRUD

---
+++ src/main/java/com/covidpersona/controller/ManagerController.java
@@ -0,0 +1,62 @@
+package com.covidpersona.controller;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.covidpersona.entity.Manager;
+import com.covidpersona.service.ManagerService;
+
+@RestController
+@RequestMapping("/managers")
+public class ManagerController {
+
+	@Autowired
+	private ManagerService managerService;
+
+	@GetMapping
+	public List<Manager> getManagers() {
+		return managerService.getManagers();
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getManager(@PathVariable long id) {
+		Optional<Manager> manager = managerService.getManager(id);
+		if (manager.isPresent()) {
+			return ResponseEntity.ok(manager);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@PostMapping
+	public ResponseEntity<Manager> addManager(@RequestBody Manager manager) throws URISyntaxException {
+		Manager savedManager = managerService.addManager(manager);
+		return ResponseEntity.created(new URI("/managers/" + savedManager.getId())).body(savedManager);
+	}
+
+	@PutMapping
+	public ResponseEntity<Manager> updateManager(@RequestBody Manager manager) {
+		Manager updatedManager = managerService.updateManager(manager);
+		return ResponseEntity.ok(updatedManager);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteManager(@PathVariable long id) {
+		managerService.deleteManager(id);
+		return ResponseEntity.ok().build();
+	}
+}
---
+++ src/main/java/com/covidpersona/service/ManagerService.java
@@ -0,0 +1,40 @@
+package com.covidpersona.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.covidpersona.entity.Manager;
+import com.covidpersona.repository.ManagerRepository;
+
+@Service
+public class ManagerService {
+
+	@Autowired
+	private ManagerRepository managerRepository;
+
+	public List<Manager> getManagers() {
+		return managerRepository.findAll();
+	}
+
+	public Optional<Manager> getManager(long id) {
+		return managerRepository.findById(id);
+	}
+
+	public Manager addManager(Manager manager) {
+		Manager savedManager = managerRepository.save(manager);
+		return savedManager;
+	}
+
+	public Manager updateManager(Manager manager) {
+		Manager updatedManager = managerRepository.save(manager);
+		return updatedManager;
+	}
+
+	public void deleteManager(long id) {
+		managerRepository.deleteById(id);
+	}
+
+}
