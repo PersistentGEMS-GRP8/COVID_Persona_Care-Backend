d230eb5fd788b5a4c736b3358c1f025183dd11f4
Binuri <binurim95@gmail.com>
2021-08-11 09:56:46 +0530

    Add CrossOrgin to HAdmin & Hospital controllers

--- src/main/java/com/covidpersona/controller/HospitalController.java
+++ src/main/java/com/covidpersona/controller/HospitalController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.covidpersona.entity.Hospital;
 import com.covidpersona.service.HospitalService;
 
+@CrossOrigin(origins={ "http://localhost:3000" })
 @RestController
 @RequestMapping("/hospitals")
 public class HospitalController {
