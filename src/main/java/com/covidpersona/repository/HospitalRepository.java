1e907d189f77cf670bd63c2dffd1fc967008202c
Binuri <binurim95@gmail.com>
2021-08-13 10:16:57 +0530

    Added already exist hospitals check

--- src/main/java/com/covidpersona/repository/HospitalRepository.java
+++ src/main/java/com/covidpersona/repository/HospitalRepository.java
@@ -1,9 +1,16 @@
 package com.covidpersona.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.covidpersona.entity.Hospital;
 
+@Repository
 public interface HospitalRepository extends JpaRepository<Hospital, Integer>{
+	
+	 @Query("SELECT p FROM Hospital p WHERE LOWER(p.hName) = LOWER(:hName)")
+	Hospital findHospitalByHName(@Param("hName") String hName);
 
 }
