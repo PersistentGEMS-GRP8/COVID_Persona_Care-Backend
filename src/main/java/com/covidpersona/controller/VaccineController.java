194604e8c8595cce6263c818560934c45304be97
Sachini Chathurangi <sachini_chathurangi@persistent.com>
2021-08-16 10:20:34 +0530

    new vaccine name

--- src/main/java/com/covidpersona/controller/VaccineController.java
+++ src/main/java/com/covidpersona/controller/VaccineController.java
@@ -2,15 +2,20 @@ package com.covidpersona.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.covidpersona.entity.Vaccine;
 import com.covidpersona.service.VaccineService;
+
 
 @RestController
 @RequestMapping("/vaccines")
@@ -20,13 +25,15 @@ public class VaccineController {
 	@Autowired
 	private VaccineService vaccineService;
 	
-//	@GetMapping
-//	public List<Vaccine> getVaccineByHosId(@RequestParam int hId){
-//		return vaccineService.getVaccineByHospId(hId);
-//	}
 
 	@GetMapping
 	public List<Vaccine> getVaccines(){
 		return vaccineService.getVaccines();
 	}
+	
+	@PostMapping
+	public Vaccine addVaccine(@Valid @RequestBody Vaccine vaccine) {
+		return vaccineService.addVaccine(vaccine);
+	}
+	
 }
