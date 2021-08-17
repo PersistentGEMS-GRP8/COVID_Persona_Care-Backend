88cd1fbf5c9f86dcab877ad4ad4c4eff40723924
Dilsha Hettiarachchi <dilshanavodi@gmail.com>
2021-08-17 13:13:35 +0530

    PatientVaccination CRUD

--- src/main/java/com/covidpersona/service/PatientVaccinationService.java
+++ src/main/java/com/covidpersona/service/PatientVaccinationService.java
@@ -1,6 +1,9 @@
 //Deepak
 package com.covidpersona.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,4 +20,22 @@ public class PatientVaccinationService {
 		PatientVaccination savedVaccination = patientVaccinationRepository.save(patientVaccination);
 		return savedVaccination;
 	}
+	
+	public List<PatientVaccination> getPatientVaccinations() {
+		return patientVaccinationRepository.findAll();
+	}
+	
+	public Optional<PatientVaccination> getPatientVaccination(int id) {
+		return patientVaccinationRepository.findById(id);
+	}
+	
+	public PatientVaccination updatePatientVaccination(PatientVaccination patientVaccination) {
+		PatientVaccination updatedVaccination = patientVaccinationRepository.save(patientVaccination);
+		return updatedVaccination;
+	}
+	
+	public void deletePatientVaccination(int id) {
+		patientVaccinationRepository.deleteById(id);
+	}
+	
 }
